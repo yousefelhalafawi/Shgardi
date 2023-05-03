5a510722a3bb9795257d8f33a5bd2e5c89903f10
yousefelhalafawi <yousefelhlfawi@gmail.com>
2023-05-04 02:23:07 +0300

    add new component2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -32,7 +33,9 @@ import { Comp6Component } from './components/comp6/comp6.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
+
 
   ],
   providers: [],
--- src/app/components/comp6/comp6.component.ts
+++ src/app/components/comp6/comp6.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {  OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-comp6',
@@ -7,4 +9,33 @@ import { Component } from '@angular/core';
 })
 export class Comp6Component {
 
+  name!: string;
+  age!: number;
+  email!: string;
+  address!: string;
+  coverLetter!: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    const emailBody = `Name: ${this.name}\nAge: ${this.age}\nEmail: ${this.email}\nAddress: ${this.address}\nCover Letter: ${this.coverLetter}`;
+    // window.open(`mailto:yousefelhlfawi@gmail.com?subject=New Job Application&body=${encodeURIComponent(emailBody)}`);
+
+    // Show success message
+    Swal.fire({
+      icon: 'success',
+      title: 'Application Submitted!',
+      text: 'Thank you for applying to our company. We will get back to you soon.'
+    });
+
+    // Clear form inputs
+    this.name = '';
+    this.age = 0;
+    this.email = '';
+    this.address = '';
+    this.coverLetter = '';
+  }
 }
